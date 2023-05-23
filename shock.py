fe0a2f4b7ef3e6fd7d90a7983d4968842a711439
gsuBsl12 <133906075+gsuBsl12@users.noreply.github.com>
2023-05-23 14:56:52 -0400

    Update shock.py
    Fixed shock not being sent because it was being converted into a value too small to send. Also fixed the voltage resetting itself to 0 at the end.

--- shock.py
+++ shock.py
@@ -6,30 +6,29 @@ import time
     # timer - How long the shock will persist for.
 
 def analog_out(value, timer):
-    
-    # Convert to milliamp measurement
-    value = value / 1000
-
     # Create a new task, called 'task', and set it equal to the output of the nidaqmx Task function\n,
     task = nidaqmx.Task()
 
     # Create a new channel to send a signal to Device 'Dev1' via pin 'ao0', with th minimum nad maximum voltage values at 0 and 5 volts respectively.
     # More info: https://nidaqmx-python.readthedocs.io/en/latest/ao_channel_collection.html?highlight=add_ao_voltage#nidaqmx._task_modules.ao_channel_collection.AOChannelCollection.add_ao_voltage_chan
-    task.ao_channels.add_ao_voltage_chan('Dev1/ao0', 0, 2)
+    task.ao_channels.add_ao_voltage_chan('Dev1/ao0', 0, 5)
 
     # Start the task.
     task.start()
 
-    # Create and send a signal of 2 volts over the channel.
+    # Create and send a signal of <value> volts over the channel.
     # More info: https://nidaqmx-python.readthedocs.io/en/latest/task.html?highlight=write#nidaqmx.task.Task.write 
     task.write(value)
     time.sleep(timer)
 
-    print("Successfully sent signal on channel ao0")
+    value = 0
+    task.write(value)
 
     # Stop the task.
     task.stop()
     task.close()
 
-# Test with 1 milliamp for 2 seconds
-# analog_out(1, 2)
+    print("Successfully sent signal on channel ao0")
+
+# Test with 2 milliamp for 2 seconds
+analog_out(2, 2)
