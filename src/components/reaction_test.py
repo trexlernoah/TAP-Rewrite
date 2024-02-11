1a7bdd9311fe26309fa3d35802c030cca606d351
Noah Trexler <trexlernoah@gmail.com>
2024-02-11 15:26:00 -0500

    mv

---
+++ src/components/reaction_test.py
@@ -0,0 +1,139 @@
+# Swiped from https://stackoverflow.com/a/66033644
+from enum import Enum
+import pygame
+import random
+pygame.init()
+
+display = pygame.display.set_mode((640, 480))
+pygame.display.set_caption("Reaction Time Test")
+
+font = pygame.font.SysFont(None, 30)
+
+text = font.render("PRESS ANY KEY TO START TEST", 0, (255,255,255))
+
+class game_state(Enum):
+    START = 1
+    READY = 2
+    HOLD = 3
+    SHOCK = 4
+
+def start_loop():
+
+    display.blit(font.render("PRESS SPACEBAR TO START TEST", 0, (255,255,255)),
+                        text.get_rect(center = display.get_rect().center))
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                print("spacebar press")
+                display.fill(pygame.Color("black"))
+                pygame.display.flip()
+                print("waiting")
+                # pygame.time.wait(7650)
+                print("done waiting")
+                running = False
+                break
+        pygame.display.flip()
+
+def ready_loop():
+
+    display.blit(font.render("GET READY!", 0, (255,255,255)),
+                         text.get_rect(center = display.get_rect().center))
+    pygame.display.flip()
+    pygame.time.wait(3700)
+    display.fill(pygame.Color("black"))
+    display.blit(font.render("PRESS SPACEBAR", 0, (255,255,255)),
+                         text.get_rect(center = display.get_rect().center))
+    pygame.display.flip()
+    timer_start = pygame.time.get_ticks()
+    print(timer_start)
+    running = True
+    while running:
+        current_time = pygame.time.get_ticks()
+        time_elapsed = current_time - timer_start
+        if time_elapsed >= 3700:
+                display.fill(pygame.Color("black"))
+                display.blit(font.render("PLEASE PRESS THE SPACEBAR", 0, (255,255,255)),
+                             text.get_rect(center = display.get_rect().center))
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                print('starting')
+                running = False
+                break
+        pygame.display.flip()
+
+def hold_loop():
+    display.fill(pygame.Color("black"))
+    display.blit(font.render("PRESS SPACEBAR", 0, (255,255,255)),
+                         text.get_rect(center = display.get_rect().center))
+    pygame.display.flip()
+
+    timer_start = pygame.time.get_ticks()
+    timer_release = timer_start + random.randint(2000, 4000)
+    running = True
+    while running:
+        current_time = pygame.time.get_ticks()
+        if current_time >= timer_release:
+            display.fill(pygame.Color("black"))
+            display.blit(font.render("RELEASE", 0, (255,0,0)),
+                         text.get_rect(center = display.get_rect().center))
+
+        for event in pygame.event.get():
+            if event.type == pygame.TEXTINPUT and event.text == ' ':
+                print('holding')
+                if current_time > timer_release + 5000:
+                    display.fill(pygame.Color("black"))
+                    display.blit(font.render("YOU WAITED TOO LONG", 0, (255,255,255)),
+                                 text.get_rect(center = display.get_rect().center))
+                    pygame.display.flip()
+                    pygame.time.wait(3700)
+                    # return go to get ready
+                    running = False
+                    break
+            elif event.type == pygame.KEYUP and event.key == pygame.K_SPACE:
+                print('released')
+                if current_time < timer_release:
+                    display.fill(pygame.Color("black"))
+                    display.blit(font.render("YOU RELEASED TOO SOON", 0, (255,255,255)),
+                                 text.get_rect(center = display.get_rect().center))
+                    pygame.display.flip()
+                    pygame.time.wait(3700)
+                    # return go to get ready
+                    running = False
+                    break
+                else:
+                    display.fill(pygame.Color("black"))
+                    display.blit(font.render("YOU WIN, YOU GET TO GIVE A SHOCK", 0, (255,255,255)),
+                                 text.get_rect(center = display.get_rect().center))
+                    pygame.display.flip()
+                    pygame.time.wait(3700)
+                    # return go to get ready
+                    running = False
+                    break
+            else:
+                print(event)
+                print(current_time)
+        pygame.display.flip()
+
+
+def run():
+    current_state = game_state.START
+
+    while True:
+        if current_state == game_state.START:
+            start_loop()
+            current_state = game_state.READY
+        elif current_state == game_state.READY:
+            ready_loop()
+            current_state = game_state.HOLD
+        elif current_state == game_state.HOLD:
+            hold_loop()
+            break
+
+        
+        display.fill(pygame.Color("black"))
+        pygame.display.flip()
+
+    # elif current_state == game_state.READY:
+
+run()
