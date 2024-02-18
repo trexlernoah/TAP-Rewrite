4ecc4672064a8556048f3e8d6a45b9d5c3801638
Noah Trexler <trexlernoah@gmail.com>
2024-02-18 14:11:14 -0500

    refactor speedometer

--- src/speedometer.py
+++ src/speedometer.py
@@ -1,16 +1,21 @@
 import pygame
 import math
 import sys
+import time
+
 pygame.init()
-size = width, height = 500, 500
+size = width, height = 1000, 800
+# display = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 display = pygame.display.set_mode(size)
 font = pygame.font.SysFont(None, 30)
+
 RADIUS = 20
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 WHITE = (255, 255, 255)
 GRAY = (225, 225, 225) 
-display.fill(GRAY)
+
+display.fill(WHITE)
 window = display.get_rect()
 center_x = window.centerx
 center_y = window.centery
@@ -25,68 +30,59 @@ def clockwiseArc(point, radius, startAngle, endAngle):
     return rect, startRad, endRad
 
 def drawCircles():
-    display.fill((225, 225, 225))
-    # Initialize circles
-    pygame.draw.circle(display, WHITE, (center_x - 225, (center_y - 60) + (center_y / 2)), RADIUS, 39)
-    pygame.draw.circle(display, WHITE, (center_x - 175, (center_y - 60) + (center_y / 2)), RADIUS, 39)
-    pygame.draw.circle(display, WHITE, (center_x - 125, (center_y - 60) + (center_y / 2)), RADIUS, 39)
-    pygame.draw.circle(display, WHITE, (center_x - 75, (center_y - 60) + (center_y / 2)), RADIUS, 39)
-    pygame.draw.circle(display, WHITE, (center_x - 25, (center_y - 60) + (center_y / 2)), RADIUS, 39)
-    pygame.draw.circle(display, WHITE, (center_x + 25, (center_y - 60) + (center_y / 2)), RADIUS, 39)
-    pygame.draw.circle(display, WHITE, (center_x + 75, (center_y - 60) + (center_y / 2)), RADIUS, 39)
-    pygame.draw.circle(display, WHITE, (center_x + 125, (center_y - 60) + (center_y / 2)), RADIUS, 39)
-    pygame.draw.circle(display, WHITE, (center_x + 175, (center_y - 60) + (center_y / 2)), RADIUS, 39)
-    pygame.draw.circle(display, WHITE, (center_x + 225, (center_y - 60) + (center_y / 2)), RADIUS, 39)
+    display.fill(GRAY)
 
-    # Initialize circle text
-    low_text = font.render("Low", 0, (0,0,0))
+    for i in range(1, 11):
+        # Draw circles
+        offset = -275 + i * 50
+        x = center_x + offset
+        y = (center_y - 60) + (center_y / 2)
+        pygame.draw.circle(display, WHITE, (x, y), RADIUS, 39)
+        
+        # Draw text
+        offset = -280 + i * 50
+        print(offset)
+        x = center_x + offset
+        y = (center_y - 30) + (center_y / 2)
+        text = font.render(str(i), 1, BLACK)
+        display.blit(text, (x, y))
+
+    low_text = font.render("Low", 1, BLACK)
     display.blit(low_text, (center_x - 230, (center_y + 10) + (center_y / 2)))
 
-    high_text = font.render("High", 0, (0,0,0))
-    display.blit(high_text, (center_x + 230, (center_y + 10) + (center_y / 2)))
-
-    circle0 = font.render("1", 0, (0,0,0))
-    display.blit(circle0, (center_x - 230, (center_y - 30) + (center_y / 2)))
-
-    circle1 = font.render("2", 0, (0,0,0))
-    display.blit(circle1, (center_x - 170, (center_y - 30) + (center_y / 2)))
-
-    circle2 = font.render("3", 0, (0,0,0))
-    display.blit(circle2, (center_x - 120, (center_y - 30) + (center_y / 2)))
-
-    circle3 = font.render("4", 0, (0,0,0))
-    display.blit(circle3, (center_x - 70, (center_y - 30) + (center_y / 2)))
-
-    circle4 = font.render("5", 0, (0,0,0))
-    display.blit(circle4, (center_x - 20, (center_y - 30) + (center_y / 2)))
-
-    circle5 = font.render("6", 0, (0,0,0))
-    display.blit(circle5, (center_x + 20, (center_y - 30) + (center_y / 2)))
-
-    circle6 = font.render("7", 0, (0,0,0))
-    display.blit(circle6, (center_x + 70, (center_y - 30) + (center_y / 2)))
-
-    circle7 = font.render("8", 0, (0,0,0))
-    display.blit(circle7, (center_x + 120, (center_y - 30) + (center_y / 2)))
-
-    circle8 = font.render("9", 0, (0,0,0))
-    display.blit(circle8, (center_x + 170, (center_y - 30) + (center_y / 2)))
-
-    circle9 = font.render("10", 0, (0,0,0))
-    display.blit(circle9, (center_x + 220, (center_y - 30) + (center_y / 2)))
+    high_text = font.render("High", 1, BLACK)
+    display.blit(high_text, (center_x + 220, (center_y + 10) + (center_y / 2)))
 
     # Initialize arc
-    pygame.draw.circle(display,(0, 0, 0), (center_x, center_y - 250), 100, width=5, draw_top_left=True, draw_top_right=True)
+    pygame.draw.circle(display, BLACK, (center_x, center_y - 250), 100, width=5, draw_top_left=True, draw_top_right=True)
 
-while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-    display.fill((255,255,255))
-    
-    # This works for now
-    pygame.draw.circle(display,(0, 0, 0), (250, 250), 100, width=5, draw_top_left=True, draw_top_right=True)
-    rect, startRad, endRad = clockwiseArc((center_x, center_y - 250), 95, 180, (180+(180/10 * 1)))
+def key_press(_key):
+    key = _key - 48
+    print("Key %d has been pressed" % key)
+    if key == 0:
+        key = 10
+    offset = -275 + key * 50
+    pygame.draw.circle(display, RED, (center_x + offset, (center_y - 60) + (center_y / 2)), RADIUS, 39)
+    rect, startRad, endRad = clockwiseArc((center_x, center_y - 250), 95, 180, (180+(180/10 * key)))
     pygame.draw.arc(display, RED, rect, startRad, endRad, 95)
-    drawCircles()
+    pygame.display.update()
+    time.sleep(1)
+    pygame.draw.circle(display, WHITE, (center_x + offset, (center_y - 60) + (center_y / 2)), RADIUS, 39)
+    pygame.draw.arc(display, GRAY, rect, startRad, endRad, 95)
+    pygame.display.update()
+
+running = True
+drawCircles()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+            elif event.key not in range(48, 58):
+                print("Please press a key from 0-9 on the keyboard")
+            else:
+                key_press(event.key)
+                # running = False
+    pygame.display.update() 
     pygame.display.flip()
